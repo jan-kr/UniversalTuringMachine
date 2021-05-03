b6485ad8505fc7fd53c5fb714fe597ee95522e9b
kressjan <kressjan@students.zhaw.ch>
2021-05-03 13:50:11 +0200

    V12 - code refactoring

--- app/src/main/java/UniversalTuringMachine/Transition.java
+++ app/src/main/java/UniversalTuringMachine/Transition.java
@@ -1,41 +1,83 @@
 package UniversalTuringMachine;
 
+/**
+ * Transition of {@link UniversalTuringMachine}. A transition is describing,
+ * what operations can be made from each step. It is used to change states
+ * according to the operation logic needed for the calculation.
+ *
+ * @author kressjan
+ * @version 1.0.0
+ */
 public class Transition {
 
-		private String state;
-		private String tapeValue, tapeValueNew;
-		private char direction;
-		private String newState;
+    private String state;
+    private String tapeValue, tapeValueNew;
+    private char direction;
+    private String newState;
 
+    /**
+     * Instantiates a new transition with the given source state, tape value, value to write
+     * to tape, direction of the r/w head and the next state of the tm.
+     *
+     * @param state        current state of machine
+     * @param tapeValue    current tape value
+     * @param tapeValueNew tape value to write
+     * @param direction    direction to move r/w head
+     * @param newState     next state of machine
+     */
+    public Transition(String state, String tapeValue, String tapeValueNew, char direction, String newState) {
+        if (direction != 'r' && direction != 'l' && direction != 'e') {
+            throw new IllegalArgumentException("Illegal direction given!");
+        }
+        this.state = state;
+        this.tapeValue = tapeValue;
+        this.tapeValueNew = tapeValueNew;
+        this.direction = direction;
+        this.newState = newState;
+    }
 
-		public Transition(String state, String tapeValue, String tapeValueNew, char direction, String newState) {
-				if(direction != 'r' && direction != 'l' && direction != 'e') {
-						throw new IllegalArgumentException("Illegal direction given!");
-				}
-				this.state = state;
-				this.tapeValue = tapeValue;
-				this.tapeValueNew = tapeValueNew;
-				this.direction = direction;
-				this.newState = newState;
-		}
+    /**
+     * Get the current state of the turing machine
+     *
+     * @return current state of turing machine
+     */
+    public String getState() {
+        return state;
+    }
 
-		public String getState() {
-				return state;
-		}
+    /**
+     * Get the tape value of the cell where the r/w head is
+     *
+     * @return tape value at current r/w head position
+     */
+    public String getTapeValue() {
+        return tapeValue;
+    }
 
-		public String getTapeValue() {
-				return tapeValue;
-		}
+    /**
+     * Get the value, which should be written to the tape
+     *
+     * @return value to write to tape
+     */
+    public String getTapeValueNew() {
+        return tapeValueNew;
+    }
 
-		public String getTapeValueNew() {
-				return tapeValueNew;
-		}
+    /**
+     * Get the direction the r/w head is moving with this transition
+     *
+     * @return transition r/w head movement direction
+     */
+    public char getDirection() {
+        return direction;
+    }
 
-		public char getDirection() {
-				return direction;
-		}
-
-		public String getNewState() {
-				return newState;
-		}
+    /**
+     * Get the next state of the turing machine
+     *
+     * @return next state of turing machine
+     */
+    public String getNewState() {
+        return newState;
+    }
 }
--- app/src/main/java/UniversalTuringMachine/TransitionImporter.java
+++ app/src/main/java/UniversalTuringMachine/TransitionImporter.java
@@ -16,48 +16,49 @@ import java.util.IllegalFormatConversionException;
  * @version 1.0.0
  */
 public class TransitionImporter {
-	private static boolean hasStartEndState = false;
+    private static boolean hasStartEndState = false;
 
-	/**
-	 * Static transition set importer. Imports transitions from file provided.
-	 * The states for the start and end state are defined as a row with two cells.
-	 * They must be provided! Else a {@link IllegalArgumentException} is thrown.
-	 * Returns the {@link HashSet<Transition>} containing all transitions available
-	 * for the given operation.
-	 * @param file csv or text file containing comma separated values for the transitions
-	 * @return {@link HashSet<Transition>} containing all {@link Transition}s for the operation to run
-	 */
-	public static HashSet<Transition> importSet(File file) {
-		HashSet<Transition> transitionSet = new HashSet<>();
-		String line;
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(file));
-			while((line = reader.readLine()) != null) {
-				hasStartEndState = addTransitionToSet(line, transitionSet);
-			}
-		} catch (IOException e) {
-				System.err.println(e.getMessage());
-		}
-		if(!hasStartEndState) {
-			throw new IllegalArgumentException("No start- and endstate definition line found! in transition file" + file.getPath());
-		}
-		return transitionSet;
-	}
+    /**
+     * Static transition set importer. Imports transitions from file provided.
+     * The states for the start and end state are defined as a row with two cells.
+     * They must be provided! Else a {@link IllegalArgumentException} is thrown.
+     * Returns the {@link HashSet<Transition>} containing all transitions available
+     * for the given operation.
+     *
+     * @param file csv or text file containing comma separated values for the transitions
+     * @return {@link HashSet<Transition>} containing all {@link Transition}s for the operation to run
+     */
+    public static HashSet<Transition> importSet(File file) {
+        HashSet<Transition> transitionSet = new HashSet<>();
+        String line;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            while ((line = reader.readLine()) != null) {
+                hasStartEndState = addTransitionToSet(line, transitionSet);
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        if (!hasStartEndState) {
+            throw new IllegalArgumentException("No start- and endstate definition line found! in transition file" + file.getPath());
+        }
+        return transitionSet;
+    }
 
-	private static boolean addTransitionToSet(String line, HashSet<Transition> transitionSet) {
-		String[] elements = line.split(",");
-		if(elements.length == 5) {
-			if(elements[3].charAt(0) == 'e') {
-				hasStartEndState = true;
-			}
-			if(elements[1].isEmpty()) {
-				elements[1] = " ";
-			}
-			if(elements[2].isEmpty()) {
-				elements[2] = " ";
-			}
-			transitionSet.add(new Transition(elements[0], elements[1], elements[2], elements[3].charAt(0), elements[4]));
-		}
-		return hasStartEndState;
-	}
+    private static boolean addTransitionToSet(String line, HashSet<Transition> transitionSet) {
+        String[] elements = line.split(",");
+        if (elements.length == 5) {
+            if (elements[3].charAt(0) == 'e') {
+                hasStartEndState = true;
+            }
+            if (elements[1].isEmpty()) {
+                elements[1] = " ";
+            }
+            if (elements[2].isEmpty()) {
+                elements[2] = " ";
+            }
+            transitionSet.add(new Transition(elements[0], elements[1], elements[2], elements[3].charAt(0), elements[4]));
+        }
+        return hasStartEndState;
+    }
 }
